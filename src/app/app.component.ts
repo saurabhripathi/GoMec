9684348a7dc033ce126744ef6137333f682a7afc
saurabh tripathi <saurabh.tripathi@quovantis.com>
2020-07-15 16:09:52 +0530

    new test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from './service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  lengthTracker: number;
+  page: number;
+  constructor(private readonly serviceService:ServiceService ){
+
+  }
+
   title = 'interview';
-}
+  count = [{name:"asd"},{name:"asd"},{name:"asd"}]
+  List :any =[]
+
+  scrollDistance='1'
+
+  ngOnInit(): void {
+    this.serviceService.collegeList().subscribe((response)=>{
+      this.page = 1
+      this.lengthTracker = response.colleges.length
+
+      if(response.colleges.length>=10)
+      {
+        this.List=response.colleges.slice(0,10)
+
+      }
+      else{
+        const length = response.colleges.length
+        this.List=response.colleges.slice(0,length)
+
+      }
+
+
+    })
+    
+  }
+  onScrollDown(){
+    if(this.lengthTracker>=10 && this.List.length<50){
+      this.serviceService.collegeList().subscribe((response)=>{
+     
+        const List=response.colleges.slice(10*this.page,(this.page+1)*(10))
+        this.List = this.List.concat(List)
+        this.page = this.page+ 1
+        
+        console.log(response)
+  
+      })
+
+    }
+    
+
+    
+  }
+} 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    InfiniteScrollModule
 
     
   ],
